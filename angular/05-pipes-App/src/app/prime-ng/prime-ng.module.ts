b361ed3165f303b41fc6831ad781ac564485ff6a
Cristhian David Montaño Vargas <crisdavidmont@gmail.com>
2024-04-26 21:45:46 -0500

    :tada: Se finaliza la sección 12 Pipes Personalizados

--- angular/05-pipes-App/src/app/prime-ng/prime-ng.module.ts
+++ angular/05-pipes-App/src/app/prime-ng/prime-ng.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
+import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
     FieldsetModule,
+    InputTextModule,
     MenubarModule,
     PanelModule,
+    TableModule,
+    ToolbarModule,
   ]
 })
 export class PrimeNgModule { }
--- angular/05-pipes-App/src/app/products/products.module.ts
+++ angular/05-pipes-App/src/app/products/products.module.ts
@@ -1,23 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
+/**Component create */
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ProductsRoutingModule } from './products-routing.module';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 
+/**Style PrimeNG */
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
+/**Pipe */
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    OrderComponent,
+    UncommonPageComponent,
+
+    // Pipe
+    CanFlyPipe,
+    ToggleCasePipe,
+    SortByPipe,
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    PrimeNgModule
+
+    PrimeNgModule,
   ]
 })
 export class ProductsModule { }
