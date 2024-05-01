48448f73b449aa803f76ec5fd043055c706c0d62
Cristhian David Montaño Vargas <crisdavidmont@gmail.com>
2024-05-01 10:27:18 -0500

    :tada: Se finaliza la sección 18: Validaciones

--- angular/07-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ angular/07-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../shared/services/validators.service';
 
 const rtx5090 = {
   name: 'RTX5090',
@@ -25,7 +26,10 @@ export class BasicPageComponent implements OnInit {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) { }
 
   ngOnInit(): void {
     this.myForm.reset(rtx5090);
@@ -53,8 +57,7 @@ export class BasicPageComponent implements OnInit {
   }
 
   isValidField(field: string): boolean | null {
-    return this.myForm.controls[field].errors
-      && this.myForm.controls[field].touched;
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   onSave(): void {
